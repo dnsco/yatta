abf54e0acf3d60c7b44c564db00add9db5ba497f
Dennis Collective <dennis.collective@gmail.com>
2020-03-31 00:24:22 -0700

    first model

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    category = models.CharField(max_length=200)
+
+
+class ActivityEvent(models.Model):
+    name = models.CharField(max_length=200)
+    time = models.DateTimeField()
+    activity = models.ForeignKey(Activity, on_delete=models.SET_NULL, null=True)
