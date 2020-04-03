80acf18d62d6fa5a67f77b469248c578cbec7b2b
Dennis Collective <dennis.collective@gmail.com>
2020-04-03 00:55:39 -0700

    a failing test :)

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+def fetch_file():
+    import os
+    import requests
+    sheet_url = os.environ['SHEET_URL']
+    return requests.get(sheet_url)
+
+
+class ImportTests(TestCase):
+    def test_can_import_csv(self):
+        self.assertFalse(fetch_file())
--- tracker/views.py
+++ tracker/views.py
@@ -1,14 +1,7 @@
-from django.http import HttpResponse
-from django.template import loader
-
+from django.shortcuts import render
 from .models import ActivityEvent
 
-
-# from django.shortcuts import render
-
-# Create your views here.
 def homePageView(request):
     events = ActivityEvent.objects.order_by('-time')
     context = {'events': events}
-    template = loader.get_template('tracker/index.html')
-    return HttpResponse(template.render(context, request))
+    return render(request, 'tracker/index.html', context)
