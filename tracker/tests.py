f5708ab573fb48b27347156ae99d1271a98b6d3e
Dennis Collective <dennis.collective@gmail.com>
2020-04-04 02:08:13 -0700

    more parsing

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,13 +1,18 @@
 from django.test import TestCase
 
 
-def fetch_file():
-    import os
+def parse_csv_at(sheet_url):
     import requests
-    sheet_url = os.environ['SHEET_URL']
-    return requests.get(sheet_url)
+    import csv
+
+    lines = requests.get(sheet_url).text.splitlines()
+    return csv.DictReader(lines)
 
 
 class ImportTests(TestCase):
     def test_can_import_csv(self):
-        self.assertFalse(fetch_file())
+        import os
+        csv = parse_csv_at(os.environ['SHEET_URL'])
+        for row in csv:
+            print(row)
+        self.assertFalse(csv)
