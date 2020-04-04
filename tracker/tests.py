52ab8482a6f435efeae65c42ec134a6ecd762f4f
Dennis Collective <dennis.collective@gmail.com>
2020-04-04 02:17:58 -0700

    make models

--- tracker/tests.py
+++ tracker/tests.py
@@ -9,10 +9,22 @@ def parse_csv_at(sheet_url):
     return csv.DictReader(lines)
 
 
+def make_models(parsed_csv):
+    from .models import Activity, ActivityEvent
+
+    for header in parsed_csv.fieldnames[1:]:
+        Activity.objects.get_or_create(category=header)
+
+    print(Activity.objects.all())
+    
+    for row in parsed_csv:
+        print(row)
+    print(ActivityEvent.objects.all())
+
 class ImportTests(TestCase):
     def test_can_import_csv(self):
         import os
         csv = parse_csv_at(os.environ['SHEET_URL'])
-        for row in csv:
-            print(row)
+        make_models(csv)
+
         self.assertFalse(csv)
